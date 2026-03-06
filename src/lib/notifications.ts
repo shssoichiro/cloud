53aaf0f347a4f1cd433115b8c777d12333f582d8
Christiaan Arnoldus <christiaan@kilocode.ai>
2026-03-06 21:52:21 +0100

    Apply suggestion from @chrarnoldus

--- src/lib/notifications.ts
+++ src/lib/notifications.ts
@@ -73,7 +73,7 @@ const normalUnconditionalNotifications: KiloNotification[] = [
       actionURL: 'https://app.kilo.ai/app-builder',
     },
     showIn: ['extension'],
-    expiresAt: '2026-03-10T00:00:00Z',
+    expiresAt: '2026-03-09T08:00:00Z',
   },
 ];
 
