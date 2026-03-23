7a48407566966f89992e75baa639226a44306bdf
Evan Jacobson <evanjacobson3@gmail.com>
2026-03-23 15:02:48 -0600

    Remove duplicate field from merge

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -329,7 +329,6 @@ export async function createOrUpdateUser(
       email: args.google_user_email,
       display_name: args.display_name ?? null,
       hosted_domain: args.hosted_domain,
-      display_name: args.display_name,
     });
 
     return savedUser;
@@ -385,7 +384,6 @@ export async function linkAccountToExistingUser(
     avatar_url: authProviderData.google_user_image_url,
     display_name: authProviderData.display_name ?? null,
     hosted_domain: authProviderData.hosted_domain,
-    display_name: authProviderData.display_name,
   });
 
   if (!linkResult.success) {
