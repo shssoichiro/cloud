45193ae202555e725111b4443a9ef59940342c83
Christiaan Arnoldus <christiaan@kilocode.ai>
2026-03-25 23:07:03 +0100

    Apply suggestion from @chrarnoldus

--- src/lib/providers/vercel/index.ts
+++ src/lib/providers/vercel/index.ts
@@ -48,7 +48,7 @@ async function getVercelRoutingPercentage() {
   }
   if (!isOpenRouterErrorRateHigh && isVercelErrorRateHigh) {
     console.error(`[getVercelRoutingPercentage] Vercel error rate is high: ${errorRate.vercel}`);
-    return 30;
+    return 10;
   }
   return 30;
 }
