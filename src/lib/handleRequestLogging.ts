95db320ddea49fa1c5cec853c1eee782b29eb7f0
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2026-03-27 19:39:56 +0100

    Allowlist for logging requests
    I want to log requests from my personal account, because you can only have one KiloClaw per account (I think)

--- src/lib/handleRequestLogging.ts
+++ src/lib/handleRequestLogging.ts
@@ -3,7 +3,30 @@ import { db } from '@/lib/drizzle';
 import { logExceptInTest } from '@/lib/utils.server';
 import { after } from 'next/server';
 import type { GatewayRequest } from '@/lib/providers/openrouter/types';
-import { isKiloAffiliatedUser } from '@/lib/isKiloAffiliatedUser';
+import { createHash } from 'node:crypto';
+
+const users = [
+  '992891e9fe987b8960a05ed0bc9cc456979d1d71410d467f212e6233dbc0a523', // christiaan
+];
+
+const organizations = [
+  '3f48333c176a29aaeeb25f3475e38511fc7184b34321a1605a3c0db54cae6df4', // kilo
+];
+
+function hash(str: string) {
+  return createHash('sha256')
+    .update('kilolog|' + str)
+    .digest('hex');
+}
+
+function isLoggingEnabledForUser(user: User | null, organizationId: string | null): boolean {
+  return (
+    user?.google_user_email.endsWith('@kilo.ai') ||
+    user?.google_user_email.endsWith('@kilocode.ai') ||
+    (!!user?.id && users.includes(hash(user.id))) ||
+    (!!organizationId && organizations.includes(hash(organizationId)))
+  );
+}
 
 export function handleRequestLogging(params: {
   clonedResponse: Response;
@@ -14,7 +37,7 @@ export function handleRequestLogging(params: {
   request: GatewayRequest;
 }) {
   const { clonedResponse, user, organization_id, provider, model, request } = params;
-  if (!isKiloAffiliatedUser(user, organization_id)) {
+  if (!isLoggingEnabledForUser(user, organization_id)) {
     return;
   }
   after(async () => {
